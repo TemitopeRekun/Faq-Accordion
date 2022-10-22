0b76e68dc8e85f125665afd12523301b42bec52a
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2022-10-22 23:35:14 +0100

    added a lot of functionality to the challenge

--- code.js
+++ code.js
@@ -2,15 +2,21 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (let i = 0; i < acc.length; i++) {
-   acc[i].addEventListener("click", function () {
-      this.classList.toggle("active");
+	acc[i].addEventListener("click", function () {
+		for (let j = 0; j < acc.length; j++) {
+			acc[j].classList.remove("active");
+			if (j != i) {
+				acc[j].nextElementSibling.style.maxHeight = null;
+			}
+		}
 
-      var panel = this.nextElementSibling;
+		this.classList.toggle("active");
+		var panel = this.nextElementSibling;
 
-      if (panel.style.display === "block") {
-         panel.style.display = "none";
-      } else {
-         panel.style.display = "block";
-      }
-   });
+		if (panel.style.maxHeight) {
+			panel.style.maxHeight = null;
+		} else {
+			panel.style.maxHeight = panel.scrollHeight + "px";
+		}
+	});
 }
